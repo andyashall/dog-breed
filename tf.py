6f5e0f656cb6d10ed8b8f109bfa90362dd5e0252
andy <andy@ashall.co>
2017-11-13 16:47:35 +0000

    now with target

--- tf.py
+++ tf.py
@@ -9,10 +9,14 @@ import os
 path = './data/ten/'
 size = (250, 250)
 train = pd.DataFrame(columns=['id', 'img'])
+labels = pd.read_csv('./data/labels.csv', index_col=0)
+
+print(labels.head())
 
 for f in os.listdir(path):
   img = image.load_img(path + f, target_size=size) 
   img = image.img_to_array(img)
-  train = train.append({'id': f, 'img': img}, ignore_index=True)
+  l = labels.loc[f.replace('.jpg', ''), 'breed']
+  train = train.append({'id': f, 'img': img, 'target': l}, ignore_index=True)
 
 print(train.head())
