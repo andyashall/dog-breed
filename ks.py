82f1b2641dbfe98c84ada2ea406ff6522e658e0f
andy <andy@ashall.co>
2017-11-14 15:53:23 +0000

    mid keras

--- ks.py
+++ ks.py
@@ -10,14 +10,19 @@ import os
 
 # Import the data
 train_path = './data/train/'
-size = (300, 300)
+test_path = './data/ten/'
+size = (200, 200)
 x = []
 y = []
+test = []
+test_ids = []
 labels = pd.read_csv('./data/labels.csv', index_col=0)
-labels['breed'] = labels['breed'].astype('category').cat.codes.astype(np.int_)
+labels['breed'] = labels['breed'].astype('category')
+labels['breed_code'] = labels['breed'].cat.codes.astype(np.int_)
 
 print(labels.head())
 
+# Train data processing
 for f in os.listdir(train_path):
   img = image.load_img(train_path + f)
   longer_side = max(img.size)
@@ -33,7 +38,7 @@ for f in os.listdir(train_path):
   )
   img = img.resize(size)
   img = np.array(image.img_to_array(img))
-  l = labels.loc[f.replace('.jpg', ''), 'breed']
+  l = labels.loc[f.replace('.jpg', ''), 'breed_code']
   x.append(img)
   y.append(l)
 
@@ -43,18 +48,45 @@ y = np.array(y, np.int_)
 print(x.shape)
 print(y.shape)
 
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.25, random_state=1001)
+# Test data processing
+for f in os.listdir(test_path):
+  img = image.load_img(test_path + f)
+  longer_side = max(img.size)
+  horizontal_padding = (longer_side - img.size[0]) / 2
+  vertical_padding = (longer_side - img.size[1]) / 2
+  img = img.crop(
+    (
+      -horizontal_padding,
+      -vertical_padding,
+      img.size[0] + horizontal_padding,
+      img.size[1] + vertical_padding
+    )
+  )
+  img = img.resize(size)
+  img = np.array(image.img_to_array(img))
+  test.append(img)
+  test_ids.append(f.replace('.jpg', ''))
+
+test = np.array(test, np.float32)
+
+print(test.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=.3, random_state=1001)
+
+print(y_test.shape)
 
 base_model = VGG19(
   weights = None,
   include_top=False,
-  input_shape=(300, 300, 3)
+  input_shape=(200, 200, 3)
 )
+
+num_class = y.shape[0]
 
 # Add a new top layer
 out = base_model.output
 out = Flatten()(out)
-predictions = Dense(120, activation='softmax')(out)
+predictions = Dense(num_class, activation='softmax')(out)
 
 # This is the model we will train
 model = Model(inputs=base_model.input, outputs=predictions)
@@ -75,3 +107,12 @@ model.summary()
 
 model.fit(X_train, y_train, epochs=1, validation_data=(X_test, y_test), verbose=1)
 
+preds = model.predict(test, verbose=1)
+
+sub = pd.DataFrame(preds)
+
+sub.columns = labels['breed'].unique()
+
+sub['id'] = test_ids
+
+sub.head(5)
